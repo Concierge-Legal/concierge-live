eb7f08b00bd89d8f48ef85156c592423479313d2
spartypkp <will@diamondquarters.com>
2024-05-17 19:56:25 -0700

    removing nav bar from chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -208,7 +208,6 @@ export default function Chat() {
 
 	return (
 		<ChatContextProvider value={{ currentlyStreamingBlockId: conciergeStreamBlockId, showLoadingIcon }}>
-			<NavBar></NavBar>
 			<div className="App flex flex-col min-h-screen bg-app">
 				<div className="flex flex-col shadow-lg rounded-lg p-6 bg-chat flex-grow">
 					<div className="flex flex-col flex-grow">
