68fc37fc1eb5001a841d06a07109c9a413766223
Madeline <datbihmad@gmail.com>
2024-05-25 08:52:31 -0400

    form complete for now

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,22 +2,20 @@ import Image from "next/image";
 import React from "react";
 import ContactForm from "@/components/contactSales";
 import NavBar from "@/components/navBar";
+import Footer from "@/components/footer";
 
 export default function Pricing() {
  
 
   return (
-    <main>
-    <NavBar></NavBar>
-    <div className="flex flex-col">
- 
-      
-    <h1>Contact Us for Pricing</h1>
-    <ContactForm />
-  </div>
-
+    <main className="min-h-screen flex flex-col">
+    <NavBar />
+    <div className="flex flex-col items-center justify-center flex-grow bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">Please contact us for pricing!</h1>
+      <ContactForm />
+    </div>
+    <Footer />
   </main>
-     
   );
 }
 
